1f29c0474d1315a7474d82e323c580d528fbaaa0
Wojciech Sklorz <Wojciech.Sklorz@agdata.com>
2024-01-22 19:32:28 +0100

    Project stub + dummy test.

---
+++ KataDiamond/KataDiamond.Tests/DiamondTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace KataDiamond.Tests
+{
+    [TestFixture]
+    public class DiamondTests
+    {
+        [Test]
+        public void DummyTest()
+        {
+            // arrange
+            char midpoint = 'A', separator = ' ';
+
+            // act
+            var diamond = new Diamond(midpoint, separator);
+            
+            // assert
+            diamond.Midpoint.Should().Be(midpoint);
+            diamond.Separator.Should().Be(separator);
+        }
+    }
+}
---
+++ KataDiamond/KataDiamond/Diamond.cs
@@ -0,0 +1,14 @@
+﻿namespace KataDiamond;
+
+public class Diamond
+{
+    public Diamond(char midpoint, char separator)
+    {
+        Midpoint = midpoint;
+        Separator = separator;
+    }
+
+    public char Separator { get; }
+
+    public char Midpoint { get; }
+}
